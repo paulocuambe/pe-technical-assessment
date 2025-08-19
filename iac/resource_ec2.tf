45d63f0ebdbe93c1cc9d50257206d65941b74e66
gosenx <pamossecuambe@gmail.com>
2025-08-19 23:34:31 +0200

    wip: use public subnet and write instance ip to an inventory file

--- iac/resource_ec2.tf
+++ iac/resource_ec2.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-02839d797c7613993" # Amazon Linux 2023 x86
   instance_type = "t3.micro"
-  subnet_id = aws_subnet.pvt.id
+  subnet_id = aws_subnet.public.id
   user_data = <<EOF
     #cloud-config
 	users:
