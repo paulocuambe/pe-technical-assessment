248343bfcd781b43a96f907d584ee791b883bfb9
gosenx <pamossecuambe@gmail.com>
2025-08-19 23:06:57 +0200

    wip: ansible, ec2 user_data

--- iac/resource_ec2.tf
+++ iac/resource_ec2.tf
@@ -4,6 +4,14 @@ resource "aws_instance" "web" {
   ami           = "ami-02839d797c7613993" # Amazon Linux 2023 x86
   instance_type = "t3.micro"
   subnet_id = aws_subnet.pvt.id
+  user_data = <<EOF
+    #cloud-config
+	users:
+		- name: ssm-user
+		  sudo: ALL(ALL) NOPASSWD:ALL # priviledge escalation without password
+		  ssh_authorized_keys:
+		  - ssh-rsa ${var.ssh_public_key}
+  EOF
 
   tags = {
     Name = "Word-Server"
