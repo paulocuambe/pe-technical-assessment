ec8757434b264f6c617a91449217ebde47197b94
gosenx <pamossecuambe@gmail.com>
2025-08-19 18:03:43 +0200

    wip: vpc setup

---
+++ iac/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region       = "af-south-1"
+  default_tags { tags = local.common_tags }
+}
---
+++ iac/resource_vpc.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "pvt" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Private VPC"
+  }
+}
+
+resource "aws_route_table" "default" {
+  vpc_id = aws_vpc.pvt.id
+
+  route {
+    cidr_block           = aws_vpc.pvt.cidr_block
+    network_interface_id = "local"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "pvt-default-rb"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.pvt.id
+
+  tags = {
+    Name = "internet-gtw"
+  }
+}
+
+resource "aws_main_route_table_association" "main" {
+  vpc_id         = aws_vpc.pvt.id
+  route_table_id = aws_route_table.default.id
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.pvt.id
+  cidr_block = "10.1.0.0/16"
+  map_public_ip_on_launch = false # making sure i don't get charged for public ips $$
+
+  tags = {
+    Name = "SUB-1-PVT"
+  }
+}
