8e3c461abb63b5035ed98aa55219154fbb8cb1d7
gosenx <pamossecuambe@gmail.com>
2025-08-20 04:22:24 +0200

    wip: allow ssh traffic

--- iac/resource_ec2.tf
+++ iac/resource_ec2.tf
@@ -31,6 +31,14 @@ resource "aws_security_group" "web_traffic" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Accept traffic to ssh port"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
